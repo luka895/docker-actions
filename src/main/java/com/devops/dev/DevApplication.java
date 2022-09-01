dfaa8d0412dd880c14438c06f4ab1c270b29478d
Lieschen Mueller <LieschenMueller@unperfekt.blog>
2022-09-01 15:02:57 +0200

    docker-gitactions

---
+++ src/main/java/com/devops/dev/DevApplication.java
@@ -0,0 +1,24 @@
+package com.devops.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DevApplication {
+
+
+   @GetMapping("/devop")
+	public String showmessage(){
+
+		return" Das scheint, alles okkkkk !";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
